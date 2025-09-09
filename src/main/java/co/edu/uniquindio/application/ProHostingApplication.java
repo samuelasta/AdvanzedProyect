6392d857952929f1eb376e2f4b7ae6d1c12de575
Samuel Castaño <samuela.castanom@uqvirtual.edu.co>
2025-09-09 00:20:04 -0500

    Commit inicial

---
+++ src/main/java/co/edu/uniquindio/application/ProHostingApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProHostingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProHostingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/uniquindio/application/ProHostingApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProHostingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
