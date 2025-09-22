040f6b3add4dc54de9c4a3b882822833d5369132
Samuel Castaño <samuela.castanom@uqvirtual.edu.co>
2025-09-22 13:21:23 -0500

    fixing details

--- src/main/java/co/edu/uniquindio/application/model/PasswordResetCode.java
+++ src/main/java/co/edu/uniquindio/application/model/PasswordResetCode.java
@@ -2,10 +2,14 @@ package co.edu.uniquindio.application.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class PasswordResetCode {
 
     @Id
--- src/main/java/co/edu/uniquindio/application/repositories/PasswordResetCodeRepository.java
+++ src/main/java/co/edu/uniquindio/application/repositories/PasswordResetCodeRepository.java
@@ -1,8 +1,9 @@
 package co.edu.uniquindio.application.repositories;
 
+import co.edu.uniquindio.application.model.PasswordResetCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PasswordResetCode extends JpaRepository<PasswordResetCode, String> {
+public interface PasswordResetCodeRepository extends JpaRepository<PasswordResetCode, String> {
 }
