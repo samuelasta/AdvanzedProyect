2af4c7bd9708811421c44750e146664cf9cd425c
Samuel Castaño <samuela.castanom@uqvirtual.edu.co>
2025-09-27 00:32:38 -0500

    .

---
+++ src/main/java/co/edu/uniquindio/application/dto/accommodationDTO/StatsDateDTO.java
@@ -0,0 +1,7 @@
+package co.edu.uniquindio.application.dto.accommodationDTO;
+
+import java.time.LocalDateTime;
+
+public record StatsDateDTO(LocalDateTime startDate,
+                           LocalDateTime endDate) {
+}
