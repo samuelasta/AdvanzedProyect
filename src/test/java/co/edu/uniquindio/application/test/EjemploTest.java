eee4debe89b2dc8dc0f969276f4cf52ba6fb6895
samuelasta <samuela.castanom@uqvirtual.edu.co>
2025-10-09 17:21:20 +0200

    gradle fixed, thanks rojo

---
+++ src/test/java/co/edu/uniquindio/application/test/EjemploTest.java
@@ -0,0 +1,14 @@
+package co.edu.uniquindio.application.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class EjemploTest {
+
+    @Test
+    public void ejemplpTest(){
+        assertEquals(1,1);
+    }
+
+}
