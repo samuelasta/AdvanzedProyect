250928f713eb59cb23ab361ef3abdf466e167e27
samuelasta <samuela.castanom@uqvirtual.edu.co>
2025-11-09 22:49:56 +0100

    Fixed the cors configuration

--- src/main/java/co/edu/uniquindio/application/config/SecurityConfig.java
+++ src/main/java/co/edu/uniquindio/application/config/SecurityConfig.java
@@ -69,7 +69,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         // Configura las políticas de CORS para permitir solicitudes desde el frontend
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("*"));
+        config.setAllowedOrigins(List.of("http://localhost:4200"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
