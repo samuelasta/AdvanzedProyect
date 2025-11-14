6d7e8dc481a6f162945f03f01541cc0c3676ee49
NataliaCardna1 <nataliatejadacardna@gmail.com>
2025-11-14 16:08:09 -0500

    Modificacion a la implementacion del servicio de email

--- src/main/java/co/edu/uniquindio/application/services/impl/EmailServiceImpl.java
+++ src/main/java/co/edu/uniquindio/application/services/impl/EmailServiceImpl.java
@@ -1,10 +1,7 @@
 package co.edu.uniquindio.application.services.impl;
 
 import co.edu.uniquindio.application.dto.externalServiceDTO.SendEmailDTO;
-import co.edu.uniquindio.application.model.Booking;
-import co.edu.uniquindio.application.model.User;
 import co.edu.uniquindio.application.services.EmailService;
-//import jakarta.validation.constraints.Email;
 import org.simplejavamail.api.email.Email;
 import org.simplejavamail.api.mailer.Mailer;
 import org.simplejavamail.api.mailer.config.TransportStrategy;
@@ -13,30 +10,36 @@ import org.simplejavamail.mailer.MailerBuilder;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
-import static org.simplejavamail.config.ConfigLoader.Property.SMTP_PORT;
-
 @Service
 public class EmailServiceImpl implements EmailService {
 
     @Override
     @Async
-    public void sendMail(SendEmailDTO sendEmailDTO) throws Exception {
-        Email email = EmailBuilder.startingBlank()
-                .from("no_reply@bookings.com")
-                .to(sendEmailDTO.recipient())
-                .withSubject(sendEmailDTO.subject())
-                .withPlainText(sendEmailDTO.body())
-                .buildEmail();
-        try(Mailer mailer = MailerBuilder
-                .withSMTPServer("smtp.gmail.com", 587, "nataliaavanzadaprogramacion@gmail.com", "bzeakckafcqoyrgi")
-                .withTransportStrategy(TransportStrategy.SMTP_TLS)
-                .withDebugLogging(true)
-                .buildMailer()) {
+    public void sendMail(SendEmailDTO sendEmailDTO) {
+        try {
+            Email email = EmailBuilder.startingBlank()
+                    .from("no_reply@bookings.com")
+                    .to(sendEmailDTO.recipient())
+                    .withSubject(sendEmailDTO.subject())
+                    .withPlainText(sendEmailDTO.body())
+                    .buildEmail();
+
+            // SIN try-with-resources
+            Mailer mailer = MailerBuilder
+                    .withSMTPServer("smtp.gmail.com", 587,
+                            "nataliaavanzadaprogramacion@gmail.com",
+                            "npkhzcipisnyfbas")
+                    .withTransportStrategy(TransportStrategy.SMTP_TLS)
+                    .withDebugLogging(true)
+                    .buildMailer();
+
             mailer.sendMail(email);
+
+            System.out.println("Email enviado exitosamente a: " + sendEmailDTO.recipient());
+
+        } catch (Exception e) {
+            System.err.println("Error al enviar email:");
+            e.printStackTrace();
         }
     }
-
-
-    }
-
-
+}
